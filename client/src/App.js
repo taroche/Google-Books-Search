bc44f237c978d080bac28d69b3e2d7a79aa64c64
Tristan LaRoche <laroche11235@gmail.com>
2021-05-31 16:43:36 -0400

    routing component set

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Nav from './components/nav';
+import saved from './components/save';
+import search from './components/search';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Nav/>
+        <Route exact path="/" component={saved}/>
+        <Route exact path="/saved" component={saved}/>
+        <Route exact path="/search" component={search}/>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
