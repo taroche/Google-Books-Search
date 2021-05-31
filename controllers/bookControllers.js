e9a190c3e32ec1d0f61088a22992e6c6f3a3c0b3
Tristan LaRoche <laroche11235@gmail.com>
2021-05-31 16:34:36 -0400

    make controller

---
+++ controllers/bookControllers.js
@@ -0,0 +1,22 @@
+const db = require("../Models");
+
+
+module.exports = {
+  findAll: function(req, res) {
+    db.Post.find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Post.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Post.findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
